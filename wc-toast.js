f2f0b6e0472c3bd074bcb9d6760f5a6b5b3b4d4a
Chris Lorenzo <Christopher_Lorenzo@Comcast.com>
2018-03-22 10:00:55 -0400

    Add Readme

--- wc-toast.js
+++ wc-toast.js
@@ -1,54 +1,64 @@
-const globalTemplates = {};
 class WCToast extends HTMLElement {
-  static get name() {
-    return 'WCToast';
-  }
-
   static get template() {
     return `
-        <style>
-          :host {
-            background-color: var(--wc-toast-background, #d84315);
-            bottom: 0;
-            box-sizing: border-box;
-            color: var(--wc-toast-color, #fff);
-            display: block;
-            opacity: 0;
-            padding: 20px 40px;
-            position: fixed;
-            text-align: left;
-            transform: translateY(100px);
-            z-index: 10000;
-          }
+      <style>
+        :host {
+          background-color: var(--wc-toast-background, #d84315);
+          bottom: 0;
+          box-sizing: border-box;
+          color: var(--wc-toast-color, #fff);
+          display: block;
+          opacity: 0;
+          padding: 20px 40px;
+          position: fixed;
+          text-align: left;
+          transform: translateY(100px);
+          z-index: 10000;
+        }
 
-          :host(.open) {
-            opacity: 1;
-            transform: translateY(-20px);
-            transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
-          }
+        :host(.open) {
+          opacity: 1;
+          transform: translateY(-20px);
+          transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
+        }
 
-          :host(.close) {
-            opacity: 0;
-            transform: translateY(100px);
-            transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
-          }
+        :host(.close) {
+          opacity: 0;
+          transform: translateY(100px);
+          transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
+        }
 
-          .close {
-            cursor: pointer;
-            position: absolute;
-            right: 4px;
-            top: 4px;
-          }
-        </style>
-        <div class="close">X</div>
-        <slot></slot>
+        .close {
+          cursor: pointer;
+          position: absolute;
+          right: 4px;
+          top: 4px;
+        }
+      </style>
+      <div class="close">X</div>
+      <slot></slot>
     `;
   }
 
+  static get cache() {
+    this._cache = this._cache || {};
+    return this._cache;
+  }
+
   constructor() {
-    super();
+    super(); // always call super() first in the constructor
+
+    let template = this.constructor.cache[this.constructor.name];
+    if (!template) {
+      template = document.createElement('template');
+      template.innerHTML = this.constructor.template;
+      this.constructor.cache[this.constructor.name] = template;
+    }
+
+    let node = template.content.cloneNode(true);
     this.root = this.attachShadow({mode: 'open'});
-    this.render();
+    this.root.appendChild(node);
+    this.root.querySelector('.close').addEventListener('click', e => this.close());
   }
 
   static get observedAttributes() {
@@ -63,27 +73,13 @@ class WCToast extends HTMLElement {
 
   open() {
     this.className = 'open';
-    this.root.querySelector('.close').addEventListener('click', e => {
-      this.close();
-    }, {once: true});
-
-    setTimeout(_ => this.close(), this.duration * 1000);
+    if (this.duration) {
+      setTimeout(_ => this.close(), this.duration * 1000);
+    }
   }
 
   close() {
     this.className = 'close';
   }
-
-  render() {
-    let template = globalTemplates[this.constructor.name];
-    if (!template) {
-      template = document.createElement('template');
-      template.innerHTML = this.constructor.template;
-      globalTemplates[this.constructor.name] = template;
-    }
-
-    const node = template.content.cloneNode(true);
-    this.root.appendChild(node);
-  }
 }
-customElements.define('wc-toast', WCToast);
+window.customElements.define('wc-toast', WCToast);
