48466dfbb5a69ae93e188d52a481a43a6fb9e0a1
Chris Lorenzo <Christopher_Lorenzo@Comcast.com>
2018-07-05 08:49:11 -0400

    Add all lifecycle hooks

--- wc-toast.js
+++ wc-toast.js
@@ -1,9 +1,18 @@
 class WCToast extends HTMLElement {
+  // Different lifecycle hooks defined by the spec!
   constructor() {
     super(); // always call super() first in the constructor
   }
 
   connectedCallback() {
+
+  }
+
+  disconnectedCallback() {
+
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
 
   }
 }
